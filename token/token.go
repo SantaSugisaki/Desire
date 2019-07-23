be27ab0270d84637ac14ef1045cf5699d65eecd3
SantaSugisaki <santakyopro@gmail.com>
2019-07-24 01:21:12 +0900

    レキサーを追加

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+type TokenType string
+
+type Token struct{
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT" //1234567
+
+	// 演算子
+	ASSIGN = "="
+	PLUS = "+"
+
+	// デリミタ
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok{
+		return tok
+	}
+	return IDENT
+}
